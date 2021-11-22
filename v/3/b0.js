9a2d3c1f083bfda8ccc46af3d956d6592fb3ff44
jisRD <54657234+jisRD@users.noreply.github.com>
2021-11-23 00:16:09 +0530

    Delete v/3 directory

--- v/3/b0.js
+++
@@ -1,2 +0,0 @@
-<div id=content><div style=display:flex><input id=button value=UPDATE type=button><input id=input value=1000></div><div id=output></div><div id=footer><a class=copy href=https://bit.ly/j-rd target=_blank><script>js2()</script></a></div></div><script>js1()</script>
-
